38cbeb6ba0822cc18bf3688c2163dc9e8a123d4c
Tristan Johns <tfj@protonmail.com>
2021-10-13 14:35:44 -0600

    draw result to page

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 function play(champion) {
   let aiChampion = "rock" // computer's pick of champion, TODO change later to random
-  let result = ""
+  // let result = ""
   // every time a champion is picked, generate a new aiChampion pick
   // if champion is rock and aiChampion is paper, champion loses, else win
   // if champion is paper and aiChampion is scissors, champion loses, else win
@@ -14,22 +14,29 @@ function play(champion) {
 
 
   if (champion == aiChampion) {
+    document.getElementById('result').innerText = 'YOU DRAW!'
     return console.log("draw");
 
 
   } else if (champion === "rock" && aiChampion === "paper") {
+    document.getElementById('result').innerText = 'YOU LOSE!'
     return console.log("lose");
   } else if (champion === "paper" && aiChampion === "scissors") {
+    document.getElementById('result').innerText = 'YOU LOSE!'
     return console.log("lose");
   } else if (champion === "scissors" && aiChampion === "rock") {
+    document.getElementById('result').innerText = 'YOU LOSE!'
     return console.log("lose");
 
 
   } else if (champion === "scissors" && aiChampion === "paper") {
+    document.getElementById('result').innerText = 'YOU WIN!'
     return console.log("win");
   } else if (champion === "rock" && aiChampion === "scissors") {
+    document.getElementById('result').innerText = 'YOU WIN!'
     return console.log("win");
   } else if (champion === "paper" && aiChampion === "rock") {
+    document.getElementById('result').innerText = 'YOU WIN!'
     return console.log("win");
   }
 }
