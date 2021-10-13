e21a1c1030253c9452984ea76c073ef3b7c480cf
Tristan Johns <tfj@protonmail.com>
2021-10-13 15:23:52 -0600

    tweaks

--- app.js
+++ app.js
@@ -1,19 +1,11 @@
 function play(champion) {
-  let aiChampion = "" // computer's pick of champion, TODO change later to random
-  // every time a champion is picked, generate a new aiChampion pick
-
-
-  // result variable is a string (win/lose/draw) that is outcome of the above logic
-
+  let aiChampion = ""
   aiChampions = ['rock', 'paper', 'scissors']
 
   let randChamp = Math.floor(Math.random() * aiChampions.length)
   aiChampion = aiChampions[randChamp]
   console.log('aiChampion:', aiChampion);
 
-  // if champion is rock and aiChampion is paper, champion loses, else win
-  // if champion is paper and aiChampion is scissors, champion loses, else win
-  // if champion is scissors and aiChampion is rock, champion loses, else win
   if (champion == aiChampion) {
     document.getElementById('ai').innerText = 'Your opponent was ' + aiChampion + ","
     document.getElementById('result').classList.remove('bg-success')
