cee1d7ad066a6dd3647ab069fe7e45aaffd1b6cd
Tristan Johns <tfj@protonmail.com>
2021-10-13 14:50:52 -0600

    draw opponent to page

--- app.js
+++ app.js
@@ -1,42 +1,58 @@
 function play(champion) {
-  let aiChampion = "rock" // computer's pick of champion, TODO change later to random
-  // let result = ""
+  let aiChampion = "" // computer's pick of champion, TODO change later to random
   // every time a champion is picked, generate a new aiChampion pick
+
+
+  // result variable is a string (win/lose/draw) that is outcome of the above logic
+
+  aiChampions = ['rock', 'paper', 'scissors']
+
+  let randChamp = Math.floor(Math.random() * aiChampions.length)
+  aiChampion = aiChampions[randChamp]
+  console.log('aiChampion:', aiChampion);
+
   // if champion is rock and aiChampion is paper, champion loses, else win
   // if champion is paper and aiChampion is scissors, champion loses, else win
   // if champion is scissors and aiChampion is rock, champion loses, else win
-
-  // I don't technically need toLowerCase(), as my input is just a button argument
-
-  // result variable is a string (win/lose/draw) that is outcome of the above logic, write this boolean to innerText interface
-
-  // outcomes = ["win", "lose", "draw"]
-
-
   if (champion == aiChampion) {
+    document.getElementById('ai').innerText = 'Your opponent was ' + aiChampion + ","
+
     document.getElementById('result').innerText = 'YOU DRAW!'
-    return console.log("draw");
+    return console.log("you draw");
 
 
   } else if (champion === "rock" && aiChampion === "paper") {
+    document.getElementById('ai').innerText = 'Your opponent was ' + aiChampion + ","
+
     document.getElementById('result').innerText = 'YOU LOSE!'
-    return console.log("lose");
+    return console.log("you lose");
   } else if (champion === "paper" && aiChampion === "scissors") {
+    document.getElementById('ai').innerText = 'Your opponent was ' + aiChampion + ","
+
     document.getElementById('result').innerText = 'YOU LOSE!'
-    return console.log("lose");
+    return console.log("you lose");
   } else if (champion === "scissors" && aiChampion === "rock") {
+    document.getElementById('ai').innerText = 'Your opponent was ' + aiChampion + ","
+
     document.getElementById('result').innerText = 'YOU LOSE!'
-    return console.log("lose");
+    return console.log("you lose");
 
 
   } else if (champion === "scissors" && aiChampion === "paper") {
+    document.getElementById('ai').innerText = 'Your opponent was ' + aiChampion + ","
+
     document.getElementById('result').innerText = 'YOU WIN!'
-    return console.log("win");
+    return console.log("you win");
   } else if (champion === "rock" && aiChampion === "scissors") {
+    document.getElementById('ai').innerText = 'Your opponent was ' + aiChampion + ","
+
     document.getElementById('result').innerText = 'YOU WIN!'
-    return console.log("win");
+    return console.log("you win");
   } else if (champion === "paper" && aiChampion === "rock") {
+    document.getElementById('ai').innerText = 'Your opponent was ' + aiChampion + ","
+
     document.getElementById('result').innerText = 'YOU WIN!'
-    return console.log("win");
+    return console.log("you win");
   }
+
 }
