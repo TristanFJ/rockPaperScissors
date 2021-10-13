c389d902de4c2101fcdde36ef106c419366b36a2
Tristan Johns <tfj@protonmail.com>
2021-10-13 15:19:23 -0600

    green if you win, red if you lose

--- app.js
+++ app.js
@@ -16,41 +16,55 @@ function play(champion) {
   // if champion is scissors and aiChampion is rock, champion loses, else win
   if (champion == aiChampion) {
     document.getElementById('ai').innerText = 'Your opponent was ' + aiChampion + ","
-
+    document.getElementById('result').classList.remove('bg-success')
+    document.getElementById('result').classList.remove('bg-danger')
+    document.getElementById('result').classList.remove('text-white')
     document.getElementById('result').innerText = 'YOU DRAW!'
     return console.log("you draw");
 
 
   } else if (champion === "rock" && aiChampion === "paper") {
     document.getElementById('ai').innerText = 'Your opponent was ' + aiChampion + ","
-
+    document.getElementById('result').classList.remove('bg-success')
+    document.getElementById('result').classList.add('text-white')
+    document.getElementById('result').classList.add('bg-danger')
     document.getElementById('result').innerText = 'YOU LOSE!'
     return console.log("you lose");
   } else if (champion === "paper" && aiChampion === "scissors") {
     document.getElementById('ai').innerText = 'Your opponent was ' + aiChampion + ","
-
+    document.getElementById('result').classList.remove('bg-success')
+    document.getElementById('result').classList.add('text-white')
+    document.getElementById('result').classList.add('bg-danger')
     document.getElementById('result').innerText = 'YOU LOSE!'
     return console.log("you lose");
   } else if (champion === "scissors" && aiChampion === "rock") {
     document.getElementById('ai').innerText = 'Your opponent was ' + aiChampion + ","
-
+    document.getElementById('result').classList.remove('bg-success')
+    document.getElementById('result').classList.add('text-white')
+    document.getElementById('result').classList.add('bg-danger')
     document.getElementById('result').innerText = 'YOU LOSE!'
     return console.log("you lose");
 
 
   } else if (champion === "scissors" && aiChampion === "paper") {
     document.getElementById('ai').innerText = 'Your opponent was ' + aiChampion + ","
-
+    document.getElementById('result').classList.remove('bg-danger')
+    document.getElementById('result').classList.add('text-white')
+    document.getElementById('result').classList.add('bg-success')
     document.getElementById('result').innerText = 'YOU WIN!'
     return console.log("you win");
   } else if (champion === "rock" && aiChampion === "scissors") {
     document.getElementById('ai').innerText = 'Your opponent was ' + aiChampion + ","
-
+    document.getElementById('result').classList.remove('bg-danger')
+    document.getElementById('result').classList.add('text-white')
+    document.getElementById('result').classList.add('bg-success')
     document.getElementById('result').innerText = 'YOU WIN!'
     return console.log("you win");
   } else if (champion === "paper" && aiChampion === "rock") {
     document.getElementById('ai').innerText = 'Your opponent was ' + aiChampion + ","
-
+    document.getElementById('result').classList.remove('bg-danger')
+    document.getElementById('result').classList.add('text-white')
+    document.getElementById('result').classList.add('bg-success')
     document.getElementById('result').innerText = 'YOU WIN!'
     return console.log("you win");
   }
